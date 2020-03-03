f351bdaf2eeab0b48a4265646263d630277b4185
Joshua Wiggins <joshua_kar@hotmail.com>
2020-03-02 21:10:16 -0400

    Added a keyPool

--- HelloWorld/Wind.h
+++ HelloWorld/Wind.h
@@ -11,6 +11,7 @@ private:
 	GLFWwindow* mainWindow;
 	GLint width, height;
 	GLint bufferWidth, bufferHeight;
+	bool keyPool[1024]; //this array will help keep track of what keys have been pressed; 0 to 1023 represends the unicode number associated with each key.
 
 public:
 	Wind();
