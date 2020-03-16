d1b10967fb03c7a0df9a27a97d823fc191cdfe2f
Joshua Wiggins <joshua_kar@hotmail.com>
2020-03-16 19:52:14 -0400

    Adds overloaded constructor and constructure

--- HelloWorld/Texture.cpp
+++ HelloWorld/Texture.cpp
@@ -1,7 +1,22 @@
 #include "Texture.h"
 
 Texture::Texture() {
+	textureId = 0;
+	width = height = bitDepth = 0;
+	fileLocation = nullptr;
+}
 
+Texture::Texture(char* fileLoc) {
+	textureId = 0;
+	width = height = bitDepth = 0;
+	fileLocation = fileLoc;
+}
+
+
+
+
+void Texture::loadTexture() {
+	const char* textureData = 
 }
 
 Texture::~Texture() {
--- HelloWorld/Texture.h
+++ HelloWorld/Texture.h
@@ -1,9 +1,19 @@
 #pragma once
-
+#include <GL/glew.h>
 
 class Texture {
+private:
+	int height;
+	int width;
+	int bitDepth;
+	GLuint textureId;
+	char* fileLocation;
 
 public:
 	Texture();
+	Texture(char* fileLoc);
+
+	void loadTexture();
+
 	~Texture();
 };
