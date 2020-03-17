6a74df7fdc0a4175c0e4b96e91da148aa6711307
Joshua Wiggins <joshua_kar@hotmail.com>
2020-03-16 20:35:44 -0400

    Adds texture load behaviour

--- HelloWorld/Texture.cpp
+++ HelloWorld/Texture.cpp
@@ -16,7 +16,33 @@ Texture::Texture(char* fileLoc) {
 
 
 void Texture::loadTexture() {
-	const char* textureData = 
+
+	//use stb to load the data
+	unsigned char* textureData = stbi_load(fileLocation, &width, &height, &bitDepth);
+
+	if (!textureData) {
+		printf("Error Loading texture.");
+	}
+
+	glGenTextures(1, &textureId);
+	glBindTexture(GL_TEXTURE_2D, textureId);
+
+	//******set some texture handling properties
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	//attach texture data to texture object
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, textureData);
+
+	//automatically generate mipmap
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	//unload texture Data
+	stbi_image_free(textureData);
 }
 
 Texture::~Texture() {
--- HelloWorld/Texture.h
+++ HelloWorld/Texture.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <GL/glew.h>
+#include "stb_image.h"
+#include <stdio.h>
 
 class Texture {
 private:
