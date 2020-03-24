ad2bb6e5f730be2bd31e1a328540e8e5e65b0c5d
Joshua Wiggins <joshua_kar@hotmail.com>
2020-03-23 21:02:14 -0400

    Prepares Light class to handle diffusion

--- HelloWorld/Light.h
+++ HelloWorld/Light.h
@@ -12,15 +12,18 @@ private:
 	//this is more of a scalar than anything -- where we are stating how much of a particular RGB will be utilized. 
 	//hence our unit values will be strictly between 0 and 1
 	glm::vec3 colour;
-
 	GLfloat ambientIntensity; //a scalar amount as well
+
+	glm::vec3 direction; //direction the light is going
+	GLfloat diffusionIntensity;
 
 
 public:
 	Light();
-	Light(GLfloat red, GLfloat green, GLfloat blue, GLfloat aIntensity);
+	Light(GLfloat red, GLfloat green, GLfloat blue, GLfloat aIntensity,
+			GLfloat xDirection, GLfloat yDirection, GLfloat zDirection, GLfloat dIntensity);
 
-	void useLight(GLfloat ambientIntensityLocation, GLfloat ambientColourLocation);
+	void useLight(GLfloat ambientIntensityLocation, GLfloat ambientColourLocation, GLfloat diffusionIntensityLocation, GLfloat lightDirectionLocation);
 
 	~Light();
 
